8bbd4c9460836e6dbbdcee717ccc9404e6e03bed
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-05-07 20:00:47 +0530

    update the type annotation

--- src/classes/Product.py
+++ src/classes/Product.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel, Field
 
 class Product(BaseModel):
     title: str
-    price: str
-    ratings: str
-    reviews: str
+    price: Optional[float]
+    ratings: float
+    reviews_count: Optional[int]
     description: Union[list[str], str]
     variants: Optional[list[dict]] = Field(default=None)
 
