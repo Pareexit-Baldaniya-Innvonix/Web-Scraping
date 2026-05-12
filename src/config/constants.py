ad1e26b5df283e590bfa9e7edcd6db4accf72ace
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-05-12 19:51:28 +0530

    update the cookie

--- src/config/constants.py
+++ src/config/constants.py
@@ -4,18 +4,18 @@ HEADERS = {
         "AppleWebKit/537.36 (KHTML, like Gecko) "
         "Chrome/120.0.0.0 Safari/537.36"
     ),
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br, zstd',
-    'Connection': 'keep-alive',
-    'Cookie': 'session-id=525-1208228-3290225; session-id-time=2082787201l; i18n-prefs=INR; lc-acbin=en_IN; csm-hit=tb:s-72M9AMM9NKGRY34YTD5D|1778067929822&t:1778067931870&adb:adblk_no; ubid-acbin=521-4822612-5201832; session-token=mDwq1bemaO0HVz9cNrdE1e3GDZJPH7W3y2KxP8ntFQC5Qd5g0wtmMRqerx2Uco68zT23rHNECf0JYzKI27dAQ0ML6sFU3Wa/E49m2qHGmRiraGjppCzoEMMW+5lbVwnhD6D4tQQRNVwnkA3Q5+yOCrfMr16oe4V2dPjlYcRyrOCa7vLIWoz1xWWJbI4Ydv2hSJBCrL1oZPQZcILpatQuQ8M3zETcmCrD; rxc=ACjGhfSn9o9gAqpl/fw',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'none',
-    'Priority': 'u=0, i',
-    'Pragma': 'no-cache',
-    'Cache-Control': 'no-cache',
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Connection": "keep-alive",
+    "Cookie": "session-id=525-1208228-3290225; session-id-time=2082787201l; i18n-prefs=INR; lc-acbin=en_IN; ubid-acbin=521-4822612-5201832; session-token=xAlyNibWcTH5csdv727SFZUbqMGFNgJlbYh/NSGtY2soZEFWGHLH/OnLkhKwOBsZ5dJUnsAzDTq+ay3BPz7T5FQRIn8CztbfzgmOhiY+lZQSgv89V513IPu0WeIQpFRWJQK4fiOltA6grHZoISIHMZIeec3mtFDS59iDOVBAVSD7X7zgfiGRHCxpkBt3pPSfUc7DNsOdqgwdt0KZf0suqY5FZGfTIZtE",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Priority": "u=0, i",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache",
 }
 
 OUTPUT_DIR = "output"
