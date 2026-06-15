87f78e303ce80d71f92ccdfd038e1c81aebaab6c
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-06-15 15:17:43 +0530

    update cookies field

--- src/config/constants.py
+++ src/config/constants.py
@@ -8,14 +8,17 @@ HEADERS = {
     "Accept-Language": "en-US,en;q=0.9",
     "Accept-Encoding": "gzip, deflate, br, zstd",
     "Connection": "keep-alive",
-    "Cookie": "session-id=525-1208228-3290225; session-id-time=2082787201l; i18n-prefs=INR; lc-acbin=en_IN; ubid-acbin=521-4822612-5201832; session-token=xAlyNibWcTH5csdv727SFZUbqMGFNgJlbYh/NSGtY2soZEFWGHLH/OnLkhKwOBsZ5dJUnsAzDTq+ay3BPz7T5FQRIn8CztbfzgmOhiY+lZQSgv89V513IPu0WeIQpFRWJQK4fiOltA6grHZoISIHMZIeec3mtFDS59iDOVBAVSD7X7zgfiGRHCxpkBt3pPSfUc7DNsOdqgwdt0KZf0suqY5FZGfTIZtE",
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
     "Sec-Fetch-Site": "none",
-    "Priority": "u=0, i",
-    "Pragma": "no-cache",
-    "Cache-Control": "no-cache",
+}
+
+COOKIES = {
+    "session-id": "521-1182317-1959339",
+    "ubid-main": "521-0304607-9496606",
+    "x-main": "kshSPnmv0jkvRckRj0EBmI?tKlodibpKCquuBCwCcfQ0h?zJQe9qK8FtL5rdOGEK",
+    "at-main": "Atza|gQBe4v5sAwEBAFMvSV13njU0ldHW9SFTTPw1MTQh4pkiKvoK68E7ltMa02Nk8chkb2uBZiVhg-eBFXoW_FMdD8vWTvyVl5OCJ9_VioJEuwksiBfaLlw_mKf1G0VDYyLpKWM5V5pKeEHWCK0nChtHtZp5tDyCmWrGNWHZTqLxNIJExGsBUuxIHUXoemKixXTSyfywhwSchF-8nNcJC5IZnoSyrqVVamFG24oNIPWEJW9MEk0cSuNU5_BNUVFqqH4_iezX3AJA7ZXu2RYn6g5DQJMKsy29Yf2yxZ99NT11JKqofpoXqB_TV3Cv_lmM8cT9Nvlo0ZXijTFpA7PIBVq_xzrz4Z4YaCVO0PoiMdV_X-c",
 }
 
 OUTPUT_DIR = "output"
