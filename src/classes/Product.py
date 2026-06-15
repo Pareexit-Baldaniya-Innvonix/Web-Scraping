a1c07b53a9ca256be5ab8ea65c46e885b377a21b
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-06-15 15:13:13 +0530

    add some extra fields

--- src/classes/Product.py
+++ src/classes/Product.py
@@ -2,14 +2,19 @@
 from typing import Optional, Union
 from pydantic import BaseModel
 
+# ----- local import -----
+from .Review import Review
+
 
 class Product(BaseModel):
     title: str
+    image: Optional[list[str]] = None
     price: Optional[float] = None
     ratings: Optional[float] = None
-    reviews_count: Optional[int] = None
+    ratings_count: Optional[int] = None
     description: Union[list[str], str]
     variants: Optional[list[dict]] = None
+    reviews: Optional[list[Review]] = None
 
     def to_dict(self) -> dict:
         return self.model_dump()
