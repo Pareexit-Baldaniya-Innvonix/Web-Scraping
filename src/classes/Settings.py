88da46a22bd27733d3f81a49ff908212c6d60d46
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-06-29 11:24:50 +0530

    add some more fields

--- src/classes/Settings.py
+++ src/classes/Settings.py
@@ -10,7 +10,8 @@ class Settings(BaseSettings):
     ENV: str = os.getenv("ENV", "development")
     AMAZON_EMAIL: Optional[str] = os.getenv("AMAZON_EMAIL", "")
     AMAZON_PASSWORD: Optional[str] = os.getenv("AMAZON_PASSWORD", "")
-    THRESHOLD_LIMIT: int = int(os.getenv("THRESHOLD_LIMIT", 2000))
+    THRESHOLD_LIMIT: int = int(os.getenv("THRESHOLD_LIMIT", 500))
+    HEADLESS: bool = os.getenv("HEADLESS", "TRUE")
 
 
 settings = Settings()
