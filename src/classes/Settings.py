4f91a1e356e308e8376ef05e12963e9ccfc9ceb1
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-06-30 17:54:43 +0530

    fix THRESHOLD_LIMIT attribute

--- src/classes/Settings.py
+++ src/classes/Settings.py
@@ -1,16 +1,15 @@
 # ----- library import -----
 import os
-from typing import Optional
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
 
-    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "DEBUG")
     ENV: str = os.getenv("ENV", "development")
-    AMAZON_EMAIL: Optional[str] = os.getenv("AMAZON_EMAIL", "")
-    AMAZON_PASSWORD: Optional[str] = os.getenv("AMAZON_PASSWORD", "")
-    THRESHOLD_LIMIT: int = int(os.getenv("THRESHOLD_LIMIT", 500))
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "DEBUG")
+    AMAZON_EMAIL: str = os.getenv("AMAZON_EMAIL", "")
+    AMAZON_PASSWORD: str = os.getenv("AMAZON_PASSWORD", "")
+    THRESHOLD_LIMIT: int = os.getenv("THRESHOLD_LIMIT", 500)
 
 
 settings = Settings()
