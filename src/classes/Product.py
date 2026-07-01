b98451276fdcd3835e2edd60e091ba0b551a3a68
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-07-01 21:15:59 +0530

    remove unnecessory attributes

--- src/classes/Product.py
+++ src/classes/Product.py
@@ -2,9 +2,6 @@
 from typing import List, Optional
 from pydantic import BaseModel
 
-# ----- local import -----
-from .Review import Review
-
 
 class Product(BaseModel):
     asin: str
@@ -15,8 +12,6 @@ class Product(BaseModel):
     ratings_count: Optional[int] = None
     description: str
     variants: Optional[List[dict]] = None
-    total_reviews: int = 0
-    reviews: List[Review]
 
     def to_dict(self) -> dict:
         return self.model_dump()
